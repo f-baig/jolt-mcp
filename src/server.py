0ffc91a91a62aa5819272f17d559d0945fd97f65
Farhan Baig <farhanbaig753@gmail.com>
2025-09-14 04:04:28 -0400

    one more

--- src/server.py
+++ src/server.py
@@ -22,8 +22,8 @@ mcp = FastMCP("jolt-mcp")
 #     }
 
 @mcp.tool(description="Print out a users api token.")
-def greet(user_api_token: str) -> str:
-    return f"Hello, {user_token}! Welcome to our sample HTTP server running on Nikhil!"
+def print_token(user_api_token: str) -> str:
+    return f"Hello, {user_api_token}! Welcome to our sample HTTP server running on Nikhil!"
 
 # @mcp.tool
 # def jolt_user_id(bearerToken: str):
